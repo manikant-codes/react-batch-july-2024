bac33bdf2073eac2faed76b7653e6c3bcb7802c6
Manikant Jha <manikantcodes@gmail.com>
2024-07-12 10:49:36 +0530

    useState counter demo

--- src/components/demo/StateDemo.jsx
+++ src/components/demo/StateDemo.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 
 function StateDemo() {
-  //   let count = 0;
+  // let count = 0;
+  // console.log("stateArray", stateArray);
+  const [count, setCount] = useState(0);
+  // OR
+  // const count = stateArray[0];
+  // const setCount = stateArray[1];
 
-  const stateArr = useState(0);
-
-  console.log("stateArr", stateArr);
-
-  const count = stateArr[0];
-  const setCount = stateArr[1];
+  function decrement() {
+    // count--;
+    // console.log(count);
+    setCount(count - 1);
+  }
 
   function increment() {
     // count++;
@@ -17,10 +21,22 @@ function StateDemo() {
   }
 
   return (
-    <div>
-      <p>{count}</p>
-      {/* <p>{count}</p> */}
-      <button onClick={increment}>increment</button>
+    <div className="flex items-center justify-center h-screen w-screen">
+      <div className="bg-indigo-200 p-8 rounded-lg flex items-center gap-6 shadow-md">
+        <button
+          onClick={decrement}
+          className="h-[30px] w-[30px] bg-indigo-600 rounded text-white text-2xl flex items-center justify-center"
+        >
+          <span className="pb-1">-</span>
+        </button>
+        <p className="text-4xl">{count}</p>
+        <button
+          onClick={increment}
+          className="h-[30px] w-[30px] bg-indigo-600 rounded text-white text-2xl flex items-center justify-center"
+        >
+          <span className="pb-1">+</span>
+        </button>
+      </div>
     </div>
   );
 }
