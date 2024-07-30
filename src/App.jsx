bdd289be68dbb23f6b5c974a2d3da762f411ff00
Manikant Jha <manikantcodes@gmail.com>
2024-07-30 16:44:34 +0530

    project cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
-import About from "./pages/About";
-import Chapter from "./pages/Chapter";
-import Contact from "./pages/Contact";
-import FAQs from "./pages/FAQs";
 import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import Verse from "./pages/Verse";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          {/* <Route index element={<UseEffectDemo />} /> */}
-          {/* <Route index element={<UseEffectDOMDemo />} /> */}
-          {/* <Route index element={<UseEffectTimerDemo />} /> */}
           <Route index element={<Home />} />
-          <Route path="chapter/:id" element={<Chapter />} />
-          <Route path="chapter/:chapterId/verse/:verseId" element={<Verse />} />
-          <Route path="about" element={<About />} />
-          <Route path="faqs" element={<FAQs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,7 @@
-import React, { useEffect, useState } from "react";
-import Hero from "../components/home/Hero";
-import VerseOfTheDay from "../components/home/VerseOfTheDay";
-import Chapters from "../components/home/Chapters";
-import { getAllChapters } from "../services/apiServices";
-import Loading from "../components/common/Loading";
+import React from "react";
 
 function Home() {
-  const [chapters, setChapters] = useState(null);
-
-  useEffect(function () {
-    getAllChapters().then((data) => {
-      setChapters(data);
-    });
-  }, []);
-
-  if (!chapters)
-    return (
-      <>
-        <Loading />
-      </>
-    );
-
-  return (
-    <>
-      <Hero url="https://bhagavadgita.io/_next/image?url=%2Fbanner2.png&w=1080&q=75" />
-      <VerseOfTheDay />
-      <Chapters chapters={chapters} />
-    </>
-  );
+  return <div>Home</div>;
 }
 
 export default Home;
