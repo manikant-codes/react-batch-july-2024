64d26fa49027c426c0048876914957e5959389b2
Manikant Jha <manikantcodes@gmail.com>
2024-07-31 11:23:55 +0530

    project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./layouts/MyLayout";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+        <Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/layouts/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const links = [
+  { id: 1, name: "Home", link: "/" },
+  { id: 2, name: "About", link: "/about" },
+  { id: 3, name: "Contact", link: "/contact" },
+];
+
+function Navbar() {
+  return (
+    <nav className="px-8 py-4 flex items-center justify-between shadow-md">
+      <h1 className="text-3xl font-bold">Bhagavad Gita</h1>
+      <ul className="flex gap-8">
+        {links.map(function (value, index, array) {
+          return (
+            <li>
+              <Link to={value.link}>{value.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
