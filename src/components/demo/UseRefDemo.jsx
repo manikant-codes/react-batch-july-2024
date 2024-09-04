40ee845869f50d9d71d30979309cfc9e052febf7
Manikant Jha <manikantcodes@gmail.com>
2024-09-04 11:08:55 +0530

    useRef second use case

--- src/components/demo/UseRefDemo.jsx
+++ src/components/demo/UseRefDemo.jsx
@@ -1,16 +1,25 @@
 import { TextInput } from "flowbite-react";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function UseRefDemo() {
   const [value, setValue] = useState("");
-  const [renderCount, setRenderCount] = useState(0);
+  // const [renderCount, setRenderCount] = useState(0);
+  const countRef = useRef(0);
+  const valueRef = useRef("");
+
+  console.log("ref", countRef);
 
   function handleChange(e) {
     setValue(e.target.value);
   }
 
+  console.log("Rendered");
+
   useEffect(() => {
-    setRenderCount(renderCount + 1);
+    // setRenderCount(renderCount + 1);
+    countRef.current += 1;
+    valueRef.current = value;
+    console.log(countRef);
   }, [value]);
 
   return (
@@ -18,7 +27,9 @@ function UseRefDemo() {
       <div className="flex flex-col gap-4">
         <div className="flex items-center gap-4 p-4 bg-orange-200 rounded-md">
           <TextInput value={value} onChange={handleChange} />
-          <p>{renderCount}</p>
+          {/* <p>{renderCount}</p> */}
+          <p>{countRef.current}</p>
+          <p>Previous value: {valueRef.current}</p>
         </div>
       </div>
     </div>
